540973d0aeaf88a80169fca6c36b72747a991c5a
Sibness <Sebiluma0202@gmail.com>
2025-05-27 13:55:38 +0200

    Update
    Updated CameraSkript, fixed some issues

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform followTransform;
-    public BoxCollider2D mapBounds;
+    public BoxCollider2D[] mapBounds;
 
     private float xMin, xMax, yMin, yMax;
     private float camX, camY;
@@ -11,27 +11,47 @@ public class CameraFollow : MonoBehaviour
     private float cameraRatio;
     private Camera mainCam;
 
+    private int pcIndex; //Index of which Level the Player is currently playing at!
+    private int oldIndex; //The last index, stops the programm from countlessly recalibrating the cam
 
     public void CalibrateCam()
     {
-        xMin = mapBounds.bounds.min.x;
-        xMax = mapBounds.bounds.max.x;
-        yMin = mapBounds.bounds.min.y;
-        yMax = mapBounds.bounds.max.y;
+        xMin = mapBounds[pcIndex].bounds.min.x;
+        xMax = mapBounds[pcIndex].bounds.max.x;
+        yMin = mapBounds[pcIndex].bounds.min.y;
+        yMax = mapBounds[pcIndex].bounds.max.y;
 
         mainCam = GetComponent<Camera>();
         camOrthsize = mainCam.orthographicSize;
-        cameraRatio = (xMax + camOrthsize) / 2.0f;
+        cameraRatio = mainCam.aspect * camOrthsize;  // FIX: Berechne die halbe Breite
+
+        oldIndex = pcIndex;
     }
     private void Start()
     {
+        pcIndex = PlayerTeleporter.CurrentIndex;
+        oldIndex = pcIndex;
         CalibrateCam();
     }
     // Update is called once per frame
     void FixedUpdate()
     {
+        pcIndex = PlayerTeleporter.CurrentIndex; //Looks if the Index has changed to update the cam if needed!
+        if (pcIndex != oldIndex)
+        {
+            CalibrateCam();
+        }
         camY = Mathf.Clamp(followTransform.position.y, yMin + camOrthsize, yMax - camOrthsize);
         camX = Mathf.Clamp(followTransform.position.x, xMin + cameraRatio, xMax - cameraRatio);
-        this.transform.position = new Vector3(camX,camY, this.transform.position.z);
+
+        // Position berechnen
+        Vector3 newPosition = new Vector3(camX, camY, transform.position.z);
+
+        // Kamera auf Pixelgrenzen runden
+        float pixelsPerUnit = 16f; // <-- Hier den Wert deiner Sprites/Tiles einsetzen!
+        newPosition.x = Mathf.Round(newPosition.x * pixelsPerUnit) / pixelsPerUnit;
+        newPosition.y = Mathf.Round(newPosition.y * pixelsPerUnit) / pixelsPerUnit;
+
+        transform.position = newPosition;
     }
 }
--- Assets/Scripts/PlayerTeleporter.cs
+++ Assets/Scripts/PlayerTeleporter.cs
@@ -3,18 +3,18 @@ using UnityEngine;
 public class PlayerTeleporter : MonoBehaviour
 {
     public Transform[] spawnpoints; //All the Spawnpoints of the player
-    private int index = 0; //The current point the player spawns
+    private static int index = 0; //The current point the player spawns
     private bool teleport = false; //Toggles if the player teleports to a spawnpoint or not
 
     public void Teleport()
     {
         teleport = true;
     }
-    public int CurrentIndex { get { return index; } }
+    public static int CurrentIndex { get { return index; } }
 
-    public void setIndex(int index)
+    public static void setIndex(int newIndex)
     { 
-        this.index = index; 
+       index = newIndex; 
     }
     void Update()
     {
